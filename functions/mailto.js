355027e49ce5124cab0130a2561795114360aaa4
Haydn Appleby <haydnba@pm.me>
2019-01-22 13:33:06 +0000

    fixes bad response code to xhr

--- functions/mailto.js
+++ functions/mailto.js
@@ -2,7 +2,7 @@ const { OWNER_EMAIL } = process.env;
 
 exports.handler = function(event, context, callback) {
     callback(null, {
-        status: 404,
+        status: 200,
         body: OWNER_EMAIL
     });
 }
