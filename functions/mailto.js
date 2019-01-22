95668648b5fd0f1b463229d8cc8d84cc7ee0a254
Haydn Appleby <haydnba@pm.me>
2019-01-22 22:45:49 +0000

    fix to get around safari blocking js mediated relocate

--- functions/mailto.js
+++ functions/mailto.js
@@ -3,6 +3,6 @@ const { OWNER_EMAIL } = process.env;
 exports.handler = function(event, context, callback) {
     callback(null, {
         status: 200,
-        body: OWNER_EMAIL
+        body: `mailto:${OWNER_EMAIL}?subject=test`
     });
 }
